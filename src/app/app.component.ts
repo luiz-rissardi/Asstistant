79548cbf9ea3bd9a394d45cbc64b15b460b39dca
luiz-rissardi <rissardi.luiz2006@gmail.com>
2024-08-26 20:48:57 -0300

    My second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, afterNextRender } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,9 +11,29 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Assistant';
 
-  constructor(){
-    afterNextRender(){
-      
-    }
+  constructor() {
+    afterNextRender(() => {
+      const recognition = new window["webkitSpeechRecognition"]();
+      recognition.continuous = true; // Continua reconhecendo mesmo após uma pausa
+      recognition.interimResults = true; // Mostra resultados intermediários
+      recognition.lang = 'pt-BR'; // Define o idioma para Português Brasileiro
+
+      recognition.start();
+      recognition.onresult = (event) => {
+        let finalTranscript = '';
+        console.log(event);
+        for (let i = event.resultIndex; i < event.results.length; i++) {
+          const transcript = event.results[i][0].transcript;
+          if (event.results[i].isFinal) {
+            finalTranscript += transcript + ' ';
+            console.log('Texto final:', finalTranscript); // Log do texto final
+          }
+        }
+      };
+
+      setTimeout(() => {
+        recognition.stop()
+      }, 3000);
+    });
   }
 }
