e41dec7ac0c46e1adfb20a9ed88208919752adc9
luiz-rissardi <rissardi.luiz2006@gmail.com>
2024-08-26 21:53:50 -0300

    Ate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,15 @@ export class AppComponent {
   constructor() {
     afterNextRender(async () => {
       // Cria uma nova instância de SpeechSynthesisUtterance
+      speechSynthesis.onvoiceschanged = () => {
+        alert("pronto para uso");
+      };
       const utterance = new SpeechSynthesisUtterance('Olá, como você está?');
       this.utterance = utterance; 
       // Define a voz e outras propriedades (opcional)
       utterance.voice = speechSynthesis.getVoices()[0] // Seleciona a primeira voz disponível
       utterance.pitch = -10; // Ajusta o tom
       utterance.rate = 2; // Ajusta a velocidade
-      // Inicia a síntese de fala 
     });
   }
   
